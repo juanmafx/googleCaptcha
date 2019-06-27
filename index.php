6424ac5a3fd390dfd6f106a7398dd057b581ec2a
Juan Manuel Ramallo <jramallo1989@gmail.com>
2019-06-27 16:41:46 -0300

    create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<title>Ejemplo reCAPTCHA 2.0 con PHP</title>
+    	<script src='https://www.google.com/recaptcha/api.js'></script>
+	</head>
+	<body>
+		<form action="exe.php" method="post">
+		    <div class="g-recaptcha" data-sitekey="API publik Key"></div>
+		    <button type="submit" name="submit">Enviar</button>
+		</form>
+	</body>
+</html>
