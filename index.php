14a428c9690c720e2a6e45c5f455aa85e62c1764
Juan Manuel Ramallo <jramallo1989@gmail.com>
2019-06-27 16:43:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     	<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
-		<form action="exe.php" method="post">
+		<form action="result.php" method="post">
 		    <div class="g-recaptcha" data-sitekey="API publik Key"></div>
 		    <button type="submit" name="submit">Enviar</button>
 		</form>
